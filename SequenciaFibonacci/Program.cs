d29d5fe7c9f39547bc3a3afb6dabb3f037c4b32e
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-03 14:05:48 -0300

    Add project files.

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿int parada, numAnterior, numPosterior, numAtual;
+
+do
+{
+    Console.WriteLine("Informe a posicao de parada (deve ser maior que zero): ");
+    parada = int.Parse(Console.ReadLine());
+} while (parada < 1);
+
+numAnterior = 1;
+numAtual = 0;
+
+for (int i = 0; i < parada; i++)
+{
+    numAtual += numAnterior;
+    numAnterior = numAtual - numAnterior;
+    Console.WriteLine(numAtual);
+}
